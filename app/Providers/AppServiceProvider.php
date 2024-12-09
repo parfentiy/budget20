745342dc9b9dc739dff5bafa5a46719d595e68ca
Your Name <you@example.com>
2024-12-09 07:14:16 +0000

    tech

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if($this->app->environment('production')) {
+        /*if($this->app->environment('production')) {
             \URL::forceScheme('https');
-        }
+    }*/
         Account::observe(AccountObserver::class);
     }
 }
